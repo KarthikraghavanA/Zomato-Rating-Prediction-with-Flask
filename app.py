1c7e1b91c9ca2804bf0b6863437babdf1d2b4b0c
Karthikraghavan <112689649+KarthikraghavanA@users.noreply.github.com>
2023-01-10 10:03:23 +0530

    Upload app and model py files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('Zomatoetmodel.pkl', 'rb'))
+model = pickle.load(open('etrmodel.pkl', 'rb'))
 
 @app.route('/')
 def home():
--- model.py
+++ model.py
@@ -1,29 +1,30 @@
 import pandas as pd
+import numpy as np
+import sklearn
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.model_selection import train_test_split
+
 import warnings
-import pickle
 warnings.filterwarnings('ignore')
 
+zData = pd.read_csv('Prepocessed_Zomato_data.csv')
+y = zData['rate']
+X = zData.drop('rate', axis=1)
 
-df = pd.read_csv('ZomatoProcessed.csv')
+xtrain, xtest, ytrain, ytest = train_test_split(X,y,test_size=.3,random_state=0)
 
-df.drop('Unnamed: 0', axis=1, inplace=True)
-print(df.head())
-x = df.drop('rate', axis=1)
-y = df.rate
+# Preparing Extra Trees Regression
 
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3, random_state=10)
+etr = ExtraTreesRegressor(n_estimators=120)
+etr.fit(xtrain, ytrain)
 
-# PREPARING EXTRA TREES REGRESSOR
+ypred = etr.predict(xtest)
 
-et = ExtraTreesRegressor(n_estimators=120)
-et.fit(xtrain, ytrain)
+print(f'Training Score',etr.score(xtrain, ytrain))
+print(f'Testing Score',etr.score(xtest, ytest))
 
-ypred = et.predict(xtest)
+import pickle
+pickle.dump(etr, open('etrmodel.pkl','wb'))
+model = pickle.load(open('etrmodel.pkl','rb'))
 
-# SAVING THE MODEL TO DISK
-pickle.dump(et, open('Zomatoetmodel.pkl', 'wb'))
-
-model = pickle.load(open('Zomatoetmodel.pkl', 'rb'))
 print(ypred)
